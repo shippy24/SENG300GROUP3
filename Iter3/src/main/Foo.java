7bcac635edd438208308ce5271bf8f8b654063f3
Tony Tea <tbtea@ucalgary.ca>
2018-04-07 11:28:24 -0600

    added counter

---
+++ Iter3/src/main/Foo.java
@@ -0,0 +1,175 @@
+//iter3
+package main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.*;
+
+import io.JavaSourceCollector;
+
+public class Foo {
+	/**
+	 * Points to a base directory of a machine</br>
+	 * @author SeungBin, Yim
+	 */
+	private static String BASEDIR = "/Users/TonyTea/Desktop/ProjectsForIter3/caleydo-develop/Ento to next/org.caleydo.vis.lineup";
+	/**
+	 * Constructor, prints out all declarations and references from sources in BASEDIR </br>	 * 
+	 * @author SeungBin, Yim
+	 */
+	
+	static ArrayList<Integer> al = new ArrayList<Integer>();
+	static ArrayList<Integer> bd = new ArrayList<Integer>();
+	static ArrayList<Integer> cz = new ArrayList<Integer>();
+	static ArrayList<Integer> it = new ArrayList<Integer>();
+	static ArrayList<Integer> pr = new ArrayList<Integer>();
+	static ArrayList<Integer> at = new ArrayList<Integer>();
+	static ArrayList<Integer> im = new ArrayList<Integer>();	
+	
+	public Foo() {
+		Map<String, Integer[]> finalMap = new HashMap();
+		File directory = new File(BASEDIR);
+		JavaSourceCollector jsc = new JavaSourceCollector(BASEDIR);
+		List<String> sourceList = null;
+		try {
+			sourceList = jsc.getSource();
+		}catch (Exception e) { e.printStackTrace(); }
+		
+		for(String source : sourceList) {
+			if(source.trim().startsWith("//")) continue;
+			Map<String, Integer[]> SourceMap = visit(parse(source));
+			for(String key : SourceMap.keySet())
+				if(finalMap.get(key) == null)
+					finalMap.put(key,  SourceMap.get(key));
+				else {
+					Integer[] value = finalMap.get(key);
+					value[0] += SourceMap.get(key)[0];
+					value[1] += SourceMap.get(key)[1];
+					finalMap.put(key, value);
+				}
+		}	
+		
+		if(finalMap == null) return;
+		//Sort keys
+		SortedSet<String> keyset = new TreeSet();
+		keyset.addAll(finalMap.keySet());
+		
+		//for(String truekey: keyset) {
+		//	System.out.printf(
+		//			"%-30s. Declarations found: %s;\treferences found: %s.\n",
+		//			truekey, finalMap.get(truekey)[0], finalMap.get(truekey)[1] );
+		//}
+	
+		
+	}
+	/**
+	 * Parses source String into AST Tree
+	 * @param sourceCode
+	 * 	Source String to be parsed by ASTParser
+	 * @return
+	 * 	Root node of a parsed AST
+	 * @author SeungBin, Yim
+	 */
+	public ASTNode parse(String sourceCode) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(sourceCode.toCharArray());
+		
+		parser.setResolveBindings(true);
+		parser.setBindingsRecovery(true);
+		
+		parser.setUnitName("");
+		
+		parser.setEnvironment(null,
+				new String[] {BASEDIR}, new String[]{"UTF-8"}, true);
+		
+		Map<String, String> options = JavaCore.getOptions();
+		options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_5); //or newer version
+		parser.setCompilerOptions(options);
+		
+		return parser.createAST(null);
+	}
+	/**
+	 * Visits AST tree, counting declarations and references
+	 * @param node
+	 * Root node of a AST
+	 * @author SeungBin, Yim
+	 */
+	public Map<String, Integer[]> visit(ASTNode node) {
+		Visitor vis = new Visitor();
+		CompilationUnit cu = (CompilationUnit)node;
+		cu.accept(vis);
+		al.add(vis.nestedCount);
+		bd.add(vis.localCount);
+		cz.add(vis.anonymousCount);
+		it.add(vis.otherInterCount);
+		pr.add(vis.primiCount);
+		at.add(vis.annotCount);
+		
+		return vis.getMap();	
+	}
+	
+	// USED TO DISPLAY COUNT ADDED 
+	public static void displayCount() {
+		int x = 0; // nested
+		int y = 0; // local
+		int z = 0; // anonymous
+		int q = 0; // otherinterfaces
+		int p = 0; // ints
+		int a = 0; // annotations
+		int i = 0; // imports
+		
+		for (int object: al) {
+			x = x + object;
+		}
+		
+		for (int object: bd) {
+			y = y + object;
+		}
+		
+		for (int object: cz) {
+			z = z + object;
+		}
+		
+		for (int object: it) {
+			q = q + object;
+		}
+		
+		for (int object: pr) {
+			p = p + object;
+		}
+		
+		for (int object: at) {
+			a = a + object;
+		}
+		
+		for (int object: im) {
+			i = i  + object;
+		}
+		
+		
+		
+		System.out.println("Nested type count: " + x);
+		System.out.println("Local type count: " + y);
+		System.out.println("Anonymous type count: " + z);
+		System.out.println("Other Interface and Class declarations: " + q); // non nested, non local, non anon
+		System.out.println("Primitve type count: " + p);
+		System.out.println("Annotation count: " + a);
+		System.out.println("Import declarations count: " + i);
+	}
+	
+	
+	//Program Entry Point
+	public static void main(String[] args) {
+	//	BASEDIR = System.getProperty("user.dir")+"\\BASEDIR";
+		new Foo();
+		displayCount();
+	}
+}
---
+++ Iter3/src/main/Visitor.java
@@ -0,0 +1,175 @@
+//iter3, now counts annotations
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jdt.core.dom.*;
+
+//Modified ASTVisitor
+/**
+ * AST Visitor for only visiting Declarations
+ * @author SeungBin, Yim
+ */
+public class Visitor extends ASTVisitor{
+	Map<String, Integer[]> map = new HashMap<String, Integer[]>();
+	
+	//public int[] counter;
+	public int nestedCount = 0;
+	public int localCount;
+	public int anonymousCount;
+	public int annotCount;
+	public int primiCount;
+	public int otherInterCount; // other interface declarations (non local, non nested, non anon)
+	public int importDecCount;
+	
+	//public Integer otherInterCount;
+	
+	public Map<String, Integer[]> getMap(){
+		return map;
+	}
+	
+	//Visits when there is a primitive type (int, char, ...)
+	@Override
+	public boolean visit(PrimitiveType node) {
+		if(!node.toString().equals("void")) {
+			String key = node.resolveBinding().getQualifiedName();
+			Integer[] count = map.get(key);
+			primiCount ++ ; 
+			if(count != null) 
+				count[1]++;
+			else
+				count = new Integer[] {0,1};
+			map.put(key, count);
+		}
+		return super.visit(node);
+	}
+	//Visits when there is a SimpleType type (non-Primitive types like java.lang.String)
+	@Override
+	public boolean visit(SimpleType node) {
+		String key = node.resolveBinding().getQualifiedName();
+		/*
+		if(node.resolveBinding().getQualifiedName().isEmpty())
+			key= node.resolveBinding().getName();
+		else
+			key = node.resolveBinding().getQualifiedName();
+			*/
+		//Not sure though
+	//	annotCount ++; 
+		if(node.getParent().toString().endsWith("[]"))
+			key += "[]";
+		//System.out.println(node.getParent());
+		Integer[] count = map.get(key);
+		if(count != null) 
+			count[1]++;
+		else
+			count = new Integer[] {0,1};
+		map.put(key, count);
+		return super.visit(node);
+	}
+	
+	//1-1. AnnotationType declaration
+	@Override
+	public boolean visit(AnnotationTypeDeclaration node) {
+		String key = node.resolveBinding().getQualifiedName();
+		Integer[] count = map.get(key);
+		annotCount ++;
+		if(count != null) 
+			count[0]++;
+		else
+			count = new Integer[] {1,0};
+		map.put(key, count);
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(MarkerAnnotation node) {
+		String key = node.resolveTypeBinding().getQualifiedName();
+		Integer[] count = map.get(key);
+		annotCount ++ ;
+		if(count != null) 
+			count[1]++;
+		else
+			count = new Integer[] {0,1};
+		map.put(key, count);
+		
+		return super.visit(node);
+	}
+
+	//2. Enum declaration
+	@Override
+	public boolean visit(EnumDeclaration node) {
+		String key = node.resolveBinding().getQualifiedName();
+		Integer[] count = map.get(key);
+	
+		if(node.resolveBinding().isNested()) {
+			nestedCount ++; 
+		}
+		if(node.resolveBinding().isLocal()) {
+			localCount ++; 
+		}
+		if(node.resolveBinding().isAnonymous()) {
+			anonymousCount ++;
+		}
+		
+		if(count != null) 
+			count[0]++;
+		else
+			count = new Integer[] {1,0};
+		map.put(key, count);
+		return super.visit(node);
+	}
+	//3-4. Class / Interface declaration
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		String key;
+		// COUNTER ADDED -
+		// counts for iter3
+		if (node.resolveBinding().isNested()) {
+			nestedCount ++; 
+		}
+		
+		if (node.resolveBinding().isLocal()) {
+			localCount ++; 
+			}
+		if (node.resolveBinding().isAnonymous()) {
+			anonymousCount ++; 
+		}
+		
+		if (!node.resolveBinding().isNested() &
+			!node.resolveBinding().isLocal()  &
+			!node.resolveBinding().isAnonymous()) {
+			otherInterCount ++ ;
+		//	System.out.println(node.resolveBinding().getName());
+		}
+		
+		if(node.resolveBinding().getQualifiedName().isEmpty())
+			key= node.resolveBinding().getName();
+		else
+			key = node.resolveBinding().getQualifiedName();
+		Integer[] count = map.get(key);
+		if(count != null) 
+			count[0]++;
+		else
+			count = new Integer[] {1,0};
+		map.put(key, count);
+		return super.visit(node);
+	}
+	//5. Import Declaration
+	@Override
+	public boolean visit(ImportDeclaration node) {
+		String key = node.resolveBinding().getName();
+		Integer[] count = map.get(key);
+		importDecCount ++ ; 
+		if(count != null) 
+			count[1]++;
+		else
+			count = new Integer[] {0,1};
+		map.put(key, count);
+		return super.visit(node);
+	}
+	
+
+
+	
+}
