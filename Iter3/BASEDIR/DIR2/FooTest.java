8609da0b74fad7a8655485cf5237a53d747d045e
Tony Tea <tbtea@ucalgary.ca>
2018-04-09 02:44:39 -0600

    Test suite now included

---
+++ Iter3/BASEDIR/DIR2/FooTest.java
@@ -0,0 +1,23 @@
+
+class FooTest {
+	int a = 3;
+	String e;
+	String[] d = {};
+	public List<List<String>> c;
+	public static void main(String args[]) {
+		List<List<String>> h = new ArrayList<ArrayList<String>>();
+		class C{
+			
+		}
+	}
+	public void test() {
+		List<List<String>> j;
+		this.a = 6;
+		class C{
+			int b = 4;
+			public C() {
+				this.b = 6;
+			}
+		}
+	}
+}
